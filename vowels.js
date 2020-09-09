6200f305ba713c93758baa76acb82e2944b9f26c
Victor Gervacio <victorgervacio33@gmail.com>
2020-09-09 09:58:23 -0600

    vowels

---
+++ vowels.js
@@ -0,0 +1,14 @@
+//Create a function that will receive a string of text of any length and return the number of vowels found within the text.
+function getWord(){
+    var word = prompt("enter a word")
+    if (word === "a" || "e"||"i"||"o"||"u"){
+            word.array.forEach(element => {
+                
+            });
+    }
+    else{
+        console.log("no vowels");
+    }
+}
+// Bonus:
+// Return the vowels used and the number of times it was used. 
